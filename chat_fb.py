f2d6fc2cd46184feac11190a5f71dfe3d02271c8
Kanik vijay <32274469+kanik9@users.noreply.github.com>
2019-09-07 20:08:23 +0530

    chat_fb.py3
    it is completely in python 3

--- chat_fb.py
+++ chat_fb.py
@@ -1,13 +1,14 @@
 import fbchat
 from getpass import getpass
-username = str(raw_input("Username: "))
+# if it must be in python 3 than use it .Otherwise don't
+username = str(input("Username: "))
 client = fbchat.Client(username, getpass())
-no_of_friends = int(raw_input("Number of friends: "))
-for i in xrange(no_of_friends):
-    name = str(raw_input("Name: "))
+no_of_friends = int(input("Number of friends: "))
+for i in range(no_of_friends):
+    name = str(input("Name: "))
     friends = client.getUsers(name)  # return a list of names
     friend = friends[0]
-    msg = str(raw_input("Message: "))
+    msg = str(input("Message: "))
     sent = client.send(friend.uid, msg)
     if sent:
         print("Message sent successfully!")
